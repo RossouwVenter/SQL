d8cbf05663a11b59e67f535dc1737d8d1a42bb3d
Rossouw <rossouw.venter.rv@gmail.com>
2021-03-29 20:18:44 +0200

    Movie app almost done

--- A Movie watchlist app/app.py
+++ A Movie watchlist app/app.py
@@ -7,13 +7,13 @@ menu = """ Please select one of the following options:
 3) View all movies
 4) Watch a movie
 5) View watched movies.
-6) Exit.
+6) Add user to the app.
+7) Exit.
 
 Your Selection: """
 welcome = "Welcome to the watchlist app!"
 
 print(welcome)
-
 database.create_tables()
 
 def promp_add_movie():
@@ -26,17 +26,28 @@ def promp_add_movie():
 
 def print_movie_list(heading,movies):
 	print("--Upcoming movies--")
-	for movie in movies:
-		movie_date = datetime.datetime.fromtimestamp(movie[1])
+	for _id,title,releas_date in movies:
+		movie_date = datetime.datetime.fromtimestamp(release_date)
 		human_date = movie_date.strftime("%b %d %Y")
-		print(f'{movie[0]} (on {human_date})')
+		print(f'{_id}: {title} (on {human_date})')
 	print("---- \n")
 
-def promt_watch_movie():
-	movie_title = input("Enter movie title you've watched: ")
-	database.watch_movie(movie_title)
+def print_watched_movie_list(username,movies):
+	print(f"--{username}'s watched movies --")
+	for movie in movies:
+		print(f'{movie[1]}')
+	print(f'---- \n')
 
-while (user_input := input(menu)) != '6':
+def promt_watch_movie():
+	username = input('Username: ')
+	movie_title = input("Movie ID: ")
+	database.watch_movie(username, movie_id)
+
+def promt_add_user():
+	username = input('Username: ')
+	database.add_user(username)
+
+while (user_input := input(menu)) != '7':
 	if user_input == '1':
 		promp_add_movie()
 	elif user_input == '2':
@@ -48,7 +59,10 @@ while (user_input := input(menu)) != '6':
 	elif user_input == '4':
 		promt_watch_movie()
 	elif user_input == '5':
-		movies = database.get_watched_movies()
-		print_movie_list("Watched", movies)
+		username = input('Username: ')
+		movies = database.get_watched_movies(username)
+		print_watched_movie_list(username, movies)
+	elif: user_input == '6'
+	prompt_add_user()
 	else:
 		print('Invalid input, please try again!')
